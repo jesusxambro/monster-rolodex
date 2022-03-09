3235d39490a7c5f48cb6724144ddcfff483f12a0
Jesus Ambro <68930496+jesusxambro@users.noreply.github.com>
2022-03-09 17:41:13 -0600

    added names

--- src/App.js
+++ src/App.js
@@ -9,31 +9,41 @@ class App extends Component {
         super();
 
         this.state = {
-            monsters: [
-              {
-                name:'Maria',
-                id: '1234',
-              },
-              {
-                name:'Neftali',
-                id: '1234a',
-              },
-              {
-                name:'Freddy',
-                id: '123s',
-              },
-              {
-                name:'Linda',
-                id: '123d',
-              },
-            ]
+            monsters: []
         };
     }
-  render() {
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response =>
+            response.json())
+            .then((users) => this.setState(() =>{
+                return {monsters: users}
+            },
+                    () => {
+                console.log(this.state);
+                    }
+                    )
+                );
+    }
+    render() {
     return (
         <div className="App">
-        {
-          this.state.monsters.map((monster) =>{
+            <input
+                className='search-box'
+                type='search'
+                placeholder='search monsters'
+                onChange={(event) => {
+                    const searchString = event.target.value.toLocaleLowerCase();
+                    const filteredMonsters = this.state.monsters.filter((monster) =>{
+                        return monster.name.toLocaleLowerCase().includes(searchString);
+                    });
+
+                    this.setState(()=>{
+                        return{ monsters: filteredMonsters};
+                    });
+                }}
+            />
+            {
+            this.state.monsters.map((monster) =>{
             return <div key={monster.id}><h1>{monster.name}</h1></div>
           })
         }
