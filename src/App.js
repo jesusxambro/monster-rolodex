8f09e7f5385ad2436558f56b0f6680200301b87f
Jesus Ambro <68930496+jesusxambro@users.noreply.github.com>
2022-03-09 20:42:39 -0600

    Optimized code in search function

--- src/App.js
+++ src/App.js
@@ -25,9 +25,20 @@ class App extends Component {
                     )
                 );
     }
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(()=>{
+            return{ searchField};
+        });
+    };
+
     render() {
-        const filteredMonsters = this.state.monsters.filter((monster) =>{
-            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        console.log('render');
+        const { monsters, searchField}= this.state;
+        const {onSearchChange} =this;
+
+        const filteredMonsters = monsters.filter((monster) =>{
+            return monster.name.toLocaleLowerCase().includes(searchField);
         });
 
     return (
@@ -36,14 +47,7 @@ class App extends Component {
                 className='search-box'
                 type='search'
                 placeholder='search monsters'
-                onChange={(event) => {
-                    const searchField = event.target.value.toLocaleLowerCase();
-
-
-                    this.setState(()=>{
-                        return{ searchField};
-                    });
-                }}
+                onChange={onSearchChange}
             />
             {filteredMonsters.map((monster) =>{
             return <div key={monster.id}><h1>{monster.name}</h1></div>
