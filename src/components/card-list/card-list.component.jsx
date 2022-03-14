bbc5f40d474d0f1e50d956cacc54860e99b28168
Jesus <jesus.ambrocio@outlook.com>
2022-03-14 13:06:16 -0500

    Fixed css and imported header font

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component} from "react";
 import './card-list.styles.css';
+import Card from '../card/card.component';
 class CardList extends Component{
     render(){
         const { monsters }= this.props;
@@ -9,15 +10,7 @@ class CardList extends Component{
                 {monsters.map((monster) => {
                    const { name, email, id} = monster;
                    return (
-                   <div className="card-container" key={id}>
-                       <img alt={'monster ${monster.name}'}
-                       src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                       />
-                       <h2>{name}</h2>
-                       <p>{email}</p>
-
-
-                   </div>
+                    <Card monster={monster} />
                 )})}
             </div>
         )
